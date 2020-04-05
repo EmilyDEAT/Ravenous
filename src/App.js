9a851ca21b02a8e9ac7188308a8fa9c86628962c
Emily Déat <emily.deat@gmail.com>
2020-04-05 19:04:29 +0200

    Adjust CSS for SearchBar

--- src/App.js
+++ src/App.js
@@ -5,11 +5,10 @@ import SearchBar from "./components/SearchBar/SearchBar";
 import "./App.css";
 
 
-
-
 function App() {
   return (
     <div className="App">
+      <h1>ravenous</h1>
       <SearchBar />
       <BusinessList />
     </div>
