50a60182f742c0fd1e9711a9c8a489d9feb3912c
Emily Déat <emily.deat@gmail.com>
2020-04-09 18:50:20 +0200

    Use Yelp API

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
 import React from "react";
 import BusinessList from "./components/BusinessList/BusinessList";
 import SearchBar from "./components/SearchBar/SearchBar";
+import Yelp from "./util/Yelp"
 
 import "./App.css";
 
+class App extends React.Component {
+  state = {
+      businesses: []
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <h1>ravenous</h1>
-      <SearchBar />
-      <BusinessList />
-    </div>
-  );
+  searchYelp = (term, location, sortBy) => {
+    Yelp.search(term, location, sortBy)
+      .then(businesses => this.setState({businesses: businesses}))
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>ravenous</h1>
+        <SearchBar search={this.searchYelp}/>
+        <BusinessList businesses={this.state.businesses}/>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -2,61 +2,16 @@ import React from "react";
 import "./BusinessList.css";
 import Business from "../Business/Business";
 
-const business = [
-  {
-  imageSrc:
-    "https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg",
-  name: "MarginOtto Pizzeria",
-  address: "1010 Paddington Way",
-  city: "Flavortown",
-  state: "NY",
-  zipCode: "10101",
-  category: "Italian",
-  rating: 4.5,
-  reviewCount: 90
-},
-{
-  imageSrc:
-    "https://cdn.pixabay.com/photo/2015/04/08/13/13/food-712665_1280.jpg",
-  name: "Teriyakiki",
-  address: "12 Salmon Street",
-  city: "New-York",
-  state: "NY",
-  zipCode: "10101",
-  category: "Japanese",
-  rating: 4.2,
-  reviewCount: 120
-},
-{
-  imageSrc:"https://cdn.pixabay.com/photo/2018/09/14/11/12/food-3676796__480.jpg",
-  name: "Le Boucher français",
-  address: "234 Long Street",
-  city: "New-York",
-  state: "NY",
-  zipCode: "10101",
-  category: "French",
-  rating: 4.5,
-  reviewCount: 56
-},
-{
-  imageSrc:"https://cdn.pixabay.com/photo/2015/04/20/13/25/burger-731298_1280.jpg",
-  name: "Burger Temple",
-  address: "1025 Steak Avenue",
-  city: "Brooklyn",
-  state: "NY",
-  zipCode: "10101",
-  category: "American",
-  rating: 3.2,
-  reviewCount: 1000
-}];
-
-const BusinessList = () => (
-  <div className="businessList-container">
-    {business.map(item => (
-      <Business {...item} />
-    ))}
-
-  </div>
-);
+class BusinessList extends React.Component {
+  render() {
+    return (
+      <div className="businessList-container">
+        {this.props.businesses.map(business => {
+          return <Business business={business} key={business.id} />;
+        })}
+      </div>
+    );
+  }
+}
 
 export default BusinessList;
